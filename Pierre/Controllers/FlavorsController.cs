7811c328a42dd4509131bd61a23c4111a5a55eb2
Kim Robinson <kimmykokonut@gmail.com>
2024-02-16 11:22:52 -0800

    add full crud for flavor

---
+++ Pierre/Controllers/FlavorsController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using Pierre.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace Pierre.Controllers;
+
+public class FlavorsController : Controller
+{
+  private readonly PierreContext _db;
+  public FlavorsController(PierreContext db)
+  {
+    _db = db;
+  }
+  public ActionResult Index()
+  {
+    List<Flavor> model = _db.Flavors.ToList();
+    return View(model);
+  }
+  public ActionResult Create()
+  {
+    return View();
+  }
+  [HttpPost]
+  public ActionResult Create(Flavor flavor)
+  {
+    if (!ModelState.IsValid)
+    {
+      return View(flavor);
+    }
+    else
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+  public ActionResult Details(int id)
+  {
+    Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+    return View(thisFlavor);
+  }
+  public ActionResult Edit(int id)
+  {
+    Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+    return View(thisFlavor);
+  }
+  [HttpPost]
+  public ActionResult Edit(Flavor flavor)
+  {
+    _db.Flavors.Update(flavor);
+    _db.SaveChanges();
+    return RedirectToAction("Details", new { id = flavor.FlavorId });
+  }
+  public ActionResult Delete(int id)
+  {
+    Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+    return View(thisFlavor);
+  }
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+    _db.Flavors.Remove(thisFlavor);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
+}
