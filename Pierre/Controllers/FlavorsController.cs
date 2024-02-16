f568f7545b9f62c3091feddfcc991f21d84d77b5
Kim Robinson <kimmykokonut@gmail.com>
2024-02-16 13:42:34 -0800

    add addtreat action and view to flavor and deletejoin on flavor details view

--- Pierre/Controllers/FlavorsController.cs
+++ Pierre/Controllers/FlavorsController.cs
@@ -71,4 +71,33 @@ public class FlavorsController : Controller
     _db.SaveChanges();
     return RedirectToAction("Index");
   }
+  public ActionResult AddTreat(int id)
+  {
+    Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+    ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+    return View(thisFlavor);
+  }
+  [HttpPost]
+  public ActionResult AddTreat(Flavor flavor, int treatId)
+  {
+#nullable enable
+    FlavorTreat? joinEntity = _db.FlavorTreats.FirstOrDefault(join => (join.TreatId == treatId && join.FlavorId == flavor.FlavorId));
+#nullable disable
+    if (joinEntity == null && treatId != 0)
+    {
+      _db.FlavorTreats.Add(new FlavorTreat() { TreatId = treatId, FlavorId = flavor.FlavorId });
+      _db.SaveChanges();
+    }
+    return RedirectToAction("Details", new { id = flavor.FlavorId });
+  }
+  [HttpPost]
+  public ActionResult DeleteTreat(int joinId, int flavorId)
+  {
+    FlavorTreat joinEntry = _db.FlavorTreats.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+    _db.FlavorTreats.Remove(joinEntry);
+    _db.SaveChanges();
+    return RedirectToAction("Details", new { id = flavorId });
+  }
+
+
 }
